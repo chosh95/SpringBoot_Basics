369d6b3534a7076465f6104ed5324b9ff2645df6
chosh95 <chosh-95@hanmail.net>
2020-06-29 16:41:20 +0900

    @SpringBootApplication Annotation

---
+++ cho-spring-boot-starter/src/main/java/me/cho/HolomanConfiguration.java
@@ -0,0 +1,20 @@
+package me.cho;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(HolomanProperties.class)
+public class HolomanConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public Holoman holoman(HolomanProperties properties ){
+        Holoman holoman = new Holoman();
+        holoman.setHowLong(properties.getHowLong());
+        holoman.setName(properties.getName());
+        return holoman;
+    }
+}
--- spring-boot-first/src/main/java/me/cho/springbootfirst/SpringBootFirstApplication.java
+++ spring-boot-first/src/main/java/me/cho/springbootfirst/SpringBootFirstApplication.java
@@ -1,7 +1,10 @@
 package me.cho.springbootfirst;
 
+import me.cho.Holoman;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class SpringBootFirstApplication {
